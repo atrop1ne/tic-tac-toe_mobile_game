20047cf3d1da9ae776c08420d736347041657e27
Anton Tropin <antropy.n@gmail.com>
2022-04-13 13:20:56 +0700

    Added game field manager script

---
+++ Assets/Scripts/GameFieldManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> fieldCellSprites;
+
+    [SerializeField]
+    private int fieldWidth = 3;
+    [SerializeField]
+    private int fieldHeight = 3;
+
+    private bool fieldAlredyGenerated = false;
+
+    public (float x, float y) GetEdgeCellsCoords()
+    {
+        return (
+            fieldWidth / 2 * fieldCellSprites[0].transform.localScale.x,
+            fieldHeight / 2 * fieldCellSprites[0].transform.localScale.y
+        );
+    }
+
+    public void GenerateField()
+    {   
+        if (!fieldAlredyGenerated)
+        {
+            (int x, int y) FieldCenterCellIndexes = (fieldWidth / 2, fieldHeight / 2);
+            for (int y = 0; y < fieldHeight; y++)
+            {
+                for (int x = 0; x < fieldWidth; x++)
+                {
+                    var current_sprite = fieldCellSprites[0];
+                    var currentCell = Instantiate(
+                        current_sprite, 
+                        new Vector2(
+                            (x - FieldCenterCellIndexes.x) * current_sprite.transform.localScale.x,
+                            (y - FieldCenterCellIndexes.y) * current_sprite.transform.localScale.y),
+                        Quaternion.identity);
+                }
+            }
+
+            fieldAlredyGenerated = true;
+        }
+    }
+}
